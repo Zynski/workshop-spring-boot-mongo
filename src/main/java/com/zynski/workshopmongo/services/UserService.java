387a8def12281a3da0c000df3e914d79ee3fd316
Zynski <leandro.conradi@gmail.com>
2019-04-15 15:30:32 -0300

    Insersao de usuario com POST

--- src/main/java/com/zynski/workshopmongo/services/UserService.java
+++ src/main/java/com/zynski/workshopmongo/services/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.zynski.workshopmongo.domain.User;
+import com.zynski.workshopmongo.dto.UserDTO;
 import com.zynski.workshopmongo.repository.UserRepository;
 import com.zynski.workshopmongo.service.exception.ObjectNotFoundException;
 
@@ -24,4 +25,13 @@ public class UserService {
 		return repo.findById(id)
 				.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
 	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(),objDto.getName(),objDto.getEmail());
+	}
+	
 }
